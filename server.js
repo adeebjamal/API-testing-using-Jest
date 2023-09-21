f56bede700b3d7969f8eaae7c9eaab1bff6676fa
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-09-21 22:14:32 +0530

    added index.html which allows the admin to create user

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use("/users", require("./Routes/read"));
 app.use("/users", require("./Routes/update"));
 app.use("/users", require("./Routes/delete"));
 
-app.listen(3000, ()=> {
+app.get("/", async(req,res)=> {
+    res.sendFile(`${__dirname}/index.html`);
+})
+
+app.listen(8000, ()=> {
     console.log("Server is running on port 3000. Go to http://localhost:3000");
 });
